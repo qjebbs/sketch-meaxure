26210fdf0564eab693112c312b58d6624d5e0c03
jebbs <qjebbs@gmail.com>
2020-04-07 17:30:50 +0800

    migrate config to sketch.Settings

--- src/meaxure/common/config.ts
+++ src/meaxure/common/config.ts
@@ -1,73 +1,60 @@
-import { deepEqual } from "../helpers/helper";
+import { sketch } from "../../sketch";
 
 export class ConfigsMaster {
-    private _prefix = "SMX3Configs";
-    private _configCache = undefined;
     private _document;
     constructor(document) {
         this._document = document;
     }
     get scale(): number {
-        return this._read<number>('scale', 1);
+        return this._readDocumentSetting<number>('scale', 1);
     }
     set scale(value: number) {
-        this._set<number>('scale', value);
+        this._setDocumentSetting<number>('scale', value);
     }
     get units(): string {
-        return this._read<string>('units', "px");
+        return this._readDocumentSetting<string>('units', "px");
     }
     set units(value: string) {
-        this._set<string>('units', value);
+        this._setDocumentSetting<string>('units', value);
     }
     get format(): string {
-        return this._read<string>('format', "color-hex");
+        return this._readDocumentSetting<string>('format', "color-hex");
     }
     set format(value: string) {
-        this._set<string>('format', value);
+        this._setDocumentSetting<string>('format', value);
     }
     get properties(): string[] {
-        return this._read<string[]>('properties', []);
+        return this._readSessionVariable<string[]>('properties', []);
     }
     set properties(value: string[]) {
-        this._set<string[]>('properties', value);
+        this._setSessionVariable<string[]>('properties', value);
     }
     get byInfluence(): boolean {
-        return this._read<boolean>('byInfluence', false);
+        return this._readSessionVariable<boolean>('byInfluence', false);
     }
     set byInfluence(value: boolean) {
-        this._set<boolean>('byInfluence', value);
+        this._setSessionVariable<boolean>('byInfluence', value);
     }
     get byPercentage(): boolean {
-        return this._read<boolean>('byPercentage', false);
+        return this._readSessionVariable<boolean>('byPercentage', false);
     }
     set byPercentage(value: boolean) {
-        this._set<boolean>('byPercentage', value);
+        this._setSessionVariable<boolean>('byPercentage', value);
     }
     get isLocked(): boolean {
-        return this._read<boolean>('isLocked', false);
+        return this._readSessionVariable<boolean>('isLocked', false);
     }
     set isLocked(value: boolean) {
-        this._set<boolean>('isLocked', value);
+        this._setSessionVariable<boolean>('isLocked', value);
     }
     get isHidden(): boolean {
-        return this._read<boolean>('isHidden', false);
+        return this._readSessionVariable<boolean>('isHidden', false);
     }
     set isHidden(value: boolean) {
-        this._set<boolean>('isHidden', value);
+        this._setSessionVariable<boolean>('isHidden', value);
     }
-    clear() {
-        this.store.removeObjectForKey(this._prefix);
-    }
-    private get store() {
-        return this._document.mutableUIMetadata();
-    }
-    private _read<T>(field: string, defaultValue: T): T {
-        if (this._configCache === undefined) {
-            this._configCache = JSON.parse(this.store.objectForKey(this._prefix));
-            // logger.debug("read config storage:", this._configCache);
-            if (this._configCache === null) this._configCache = {};
-        }
-        let value = this._configCache[field];
+    private _readDocumentSetting<T>(field: string, defaultValue: T): T {
+        let value = sketch.Settings.documentSettingForKey<T>(this._document, field);
         // logger.debug(`read config: "${field}"=${value}`);
         if (value === undefined) {
             // logger.debug(`use default ${defaultValue} for "${field}"`);
@@ -75,13 +62,19 @@ export class ConfigsMaster {
         }
         return value;
     }
-    private _set<T>(field: string, value: T) {
-        if (deepEqual(this._configCache[field], value)) return;
-        this._configCache[field] = value;
-        // logger.debug(`save to storage due to: "${field}"=>${value}`);
-        this._saveCacheToStorage();
+    private _setDocumentSetting<T>(field: string, value: T) {
+        sketch.Settings.setDocumentSettingForKey(this._document, field, value);
     }
-    private _saveCacheToStorage() {
-        this.store.setObject_forKey(JSON.stringify(this._configCache), this._prefix)
+    private _readSessionVariable<T>(field: string, defaultValue: T): T {
+        let value = sketch.Settings.sessionVariable<T>(field);
+        // logger.debug(`read config: "${field}"=${value}`);
+        if (value === undefined) {
+            // logger.debug(`use default ${defaultValue} for "${field}"`);
+            return defaultValue;
+        }
+        return value;
+    }
+    private _setSessionVariable<T>(field: string, value: T) {
+        sketch.Settings.setSessionVariable(field, value);
     }
 }
