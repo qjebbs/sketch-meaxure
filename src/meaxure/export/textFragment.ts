30042c248876b8524a7acba8a180cc7a7038648b
jebbs <qjebbs@gmail.com>
2020-04-15 20:57:30 +0800

    handle text fragments with different alignments

--- src/meaxure/export/textFragment.ts
+++ src/meaxure/export/textFragment.ts
@@ -6,6 +6,7 @@ import { ArtboardData } from "../interfaces"; import { sketch } from "../../sket
 import { tempCreatedLayers } from ".";
 import { getLayerData } from "./layerData";
 import { TextFragment } from "../../sketch/text/textFragment";
+import { Edge, EdgeVertical } from "../../sketch/layer/alignment";
 
 export function getTextFragment(artboard: Artboard, layer: Text, data: ArtboardData) {
     if (layer.type != sketch.Types.Text) return;
@@ -22,6 +23,10 @@ export function getTextFragment(artboard: Artboard, layer: Text, data: ArtboardD
     } else {
         lines = [fragments];
     }
+    let textGroup: Group = new sketch.Group({ parent: artboard });
+    textGroup.hidden = true;
+    tempCreatedLayers.push(textGroup);
+
     for (let frags of lines) {
         if (frags == null) {
             // it's a new paragraph
@@ -29,6 +34,9 @@ export function getTextFragment(artboard: Artboard, layer: Text, data: ArtboardD
             continue;
         }
         let offsetFragmentsX = 0;
+        let lineGroup = new sketch.Group({ parent: textGroup });
+        lineGroup.frame.x = 0;
+        lineGroup.frame.y = offsetFragmentsY;
         let currentLineHeight = layer.style.lineHeight || Math.max(...frags.map(f => f.defaultLineHeight));
         for (let fragment of frags) {
             fragment.style.fills.forEach(fill => {
@@ -36,18 +44,68 @@ export function getTextFragment(artboard: Artboard, layer: Text, data: ArtboardD
                 // https://github.com/sketch-hq/SketchAPI/issues/726
                 if (fill.pattern && fill.pattern.image === null) fill.pattern.image = undefined;
             });
-            let subText = new sketch.Text({ text: fragment.text, parent: layer.parent });
-            tempCreatedLayers.push(subText);
+            let subText = new sketch.Text({ text: fragment.text, parent: lineGroup });
             subText.style = fragment.style;
             subText.style.lineHeight = currentLineHeight;
-            subText.frame.x = textFrame.x + offsetFragmentsX;
-            subText.frame.y = textFrame.y + offsetFragmentsY;
+            subText.frame.x = offsetFragmentsX;
+            subText.frame.y = 0;
             offsetFragmentsX += subText.frame.width;
-            getLayerData(artboard, subText, data, false);
-            subText.remove();
         }
+        lineGroup.adjustToFit();
         offsetFragmentsY += currentLineHeight;
     }
+    switch (layer.style.alignment) {
+        case sketch.Text.Alignment.center:
+            for (let line of textGroup.layers) {
+                line.alignTo(
+                    layer,
+                    { from: Edge.center, to: Edge.center },
+                    false
+                )
+            }
+            break;
+        case sketch.Text.Alignment.right:
+            for (let line of textGroup.layers) {
+                line.alignTo(
+                    layer,
+                    { from: Edge.right, to: Edge.right },
+                    false
+                )
+            }
+            break;
+        default:
+            break;
+    }
+    textGroup.adjustToFit();
+    switch (layer.style.verticalAlignment) {
+        case sketch.Text.VerticalAlignment.top:
+            textGroup.alignTo(
+                layer,
+                { from: Edge.left, to: Edge.left },
+                { from: EdgeVertical.top, to: EdgeVertical.top },
+            )
+            break;
+        case sketch.Text.VerticalAlignment.center:
+            textGroup.alignTo(
+                layer,
+                { from: Edge.left, to: Edge.left },
+                { from: EdgeVertical.middle, to: EdgeVertical.middle },
+            )
+            break;
+        case sketch.Text.VerticalAlignment.bottom:
+            textGroup.alignTo(
+                layer,
+                { from: Edge.left, to: Edge.left },
+                { from: EdgeVertical.bottom, to: EdgeVertical.bottom },
+            )
+            break;
+        default:
+            break;
+    }
+    for (let text of sketch.find<Text>('Text', textGroup)) {
+        getLayerData(artboard, text, data, false);
+    }
+    textGroup.remove();
 }
 function getFragmentsByLines(layer: Text, fragments: TextFragment[]): TextFragment[][] {
     let svg = (
@@ -101,22 +159,24 @@ function splitFragment(fragment: TextFragment, splitText: string): [TextFragment
 }
 
 function getFragmentLinesFromSVG(svg: string) {
-    const REG_TSPAN = /<tspan x="(\d+(?:\.\d+)?)".+?>(.+?)<\/tspan>/g
-    let leftX = undefined;
+    const REG_TSPAN = /<tspan x="(\d+(?:\.\d+)?)" y="(\d+(?:\.\d+)?)" .+?>(.+?)<\/tspan>/g
+    let curY = undefined;
     let lines: { elements: string[] }[] = [];
     let lineElements: string[] = [];
     let execArray: RegExpExecArray;
     while (execArray = REG_TSPAN.exec(svg)) {
         let x = parseFloat(execArray[1]);
-        let text = execArray[2];
-        if (leftX === undefined) {
-            leftX = x;
-        } else if (leftX == x) {
+        let y = parseFloat(execArray[2]);
+        let text = execArray[3];
+        if (curY === undefined) {
+            curY = y;
+        } else if (curY !== y) {
             // next line now
             lines.push({
                 elements: lineElements,
             });
             lineElements = [text];
+            curY = y;
             continue;
         }
         // current line
