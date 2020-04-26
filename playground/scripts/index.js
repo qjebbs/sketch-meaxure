f3fb32feb77a5274fbca6892d9fd1be9e6673239
jebbs <qjebbs@gmail.com>
2020-04-26 15:18:49 +0800

    Playground console output optimize

--- playground/scripts/index.js
+++ playground/scripts/index.js
@@ -25,21 +25,33 @@ const compiler = webpack({
         filename: '[name]_bundle.js',
     },
 });
+console.log(new Date().toLocaleString() + ' Compiling...');
 const outputFileSystem = new webpack.MemoryOutputFileSystem()
 compiler.outputFileSystem = outputFileSystem;
 compiler.run((err, stats) => {
     let content = outputFileSystem.readFileSync(__dirname + '/index_bundle.js').toString();
+    console.log(new Date().toLocaleString() + ' Minifying script...');
     content = uglifyjs.minify(content, {
         mangle: {
             debug: ''
         },
         fromString: true,
     }).code;
+    console.log(new Date().toLocaleString() + ' Running...');
     let returns = child_process.spawnSync('sh', [
         __dirname + '/sketchRunScript.sh',
         encodeURIComponent(content)
     ]);
     if (returns.stdout && returns.stdout.length) {
-        console.log(returns.stdout.toString());
+        console.log(new Date().toLocaleString() + ' Done with output:');
+        console.log(indent(returns.stdout.toString(), 2));
+    } else {
+        console.log(new Date().toLocaleString() + ' Done.');
     }
 });
+
+function indent(content, indentCount) {
+    return content.split('\n')
+        .map(line => ' '.repeat(indentCount) + line)
+        .join('\n');
+}
