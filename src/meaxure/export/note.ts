7985b2102a8ed3248e8ff348bd6cc570addfbff4
jebbs <qjebbs@gmail.com>
2020-09-02 14:27:45 +0800

    hide note inside symbol, #21

--- src/meaxure/export/note.ts
+++ src/meaxure/export/note.ts
@@ -6,12 +6,13 @@ import { SMNote } from "../interfaces";
 import { sketch } from "../../sketch";
 import { toHTMLEncode, emojiToEntities } from "../helpers/helper";
 
-export function makeNote(layer: Layer, artboard: Artboard): SMNote {
+export function makeNote(layer: Layer, artboard: Artboard, symbolLayer?: Layer): SMNote {
     if (!layer || layer.type != sketch.Types.Group || !layer.name.startsWith('#meaxure-note-')) return undefined;
     let textLayers = sketch.find<Text>('Text', layer as Group);
     if (!textLayers.length) return undefined;
     let textLayer: Text = textLayers[0];
-    layer.hidden = true;
+    if (!layer.hidden) layer.hidden = true;
+    if (symbolLayer && !symbolLayer.hidden) symbolLayer.hidden = true;
     return <SMNote>{
         rect: layer.frame.changeBasis({ from: layer.parent as Group, to: artboard }),
         note: toHTMLEncode(emojiToEntities(textLayer.text)).replace(/\n/g, "<br>"),
