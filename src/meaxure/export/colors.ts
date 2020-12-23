70de5b9d05e6614b5fb0e5f345c384ce77401cea
jebbs <qjebbs@gmail.com>
2020-12-23 14:44:07 +0800

    switch to swatches, #55

--- src/meaxure/export/colors.ts
+++ src/meaxure/export/colors.ts
@@ -6,8 +6,8 @@ import { SMColorAsset } from "../interfaces";
 import { parseColor } from "../helpers/styles";
 
 export function getDocumentColors(document: Document): SMColorAsset[] {
-    return document.colors.map(colorAsset => ({
-        name: colorAsset.name,
-        color: parseColor(colorAsset.color),
+    return document.swatches.map(s => ({
+        name: s.name,
+        color: parseColor(s.color),
     }));
 }
